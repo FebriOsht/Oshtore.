5f1b62a0a810c04a70ea367d8acccc9c87b66a03
Febri Osht <103566292+FebriOsht@users.noreply.github.com>
2026-03-03 10:43:50 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const jakarta = Plus_Jakarta_Sans({
 });
 
 export const metadata: Metadata = {
-  title: 'Oshtore | Jasa Landing Page & Sistem Bisnis Profesional',
+  title: 'Oshtore',
   description: 'Tingkatkan konversi penjualan dan efisiensi operasional bisnis Anda dengan solusi digital dari Oshtore.',
   // Definisi ikon yang lebih lengkap agar menimpa default browser/Vercel
   icons: {
